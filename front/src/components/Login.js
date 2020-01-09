b2698c39f76a86e2ed905023d9517e4af43e78f2
Sandor Korotkevics <korotkevics.sandor@gmail.com>
2020-01-10 00:36:17 +0100

    front: Sketch up axios request.

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,15 +1,18 @@
 import React, {Component} from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios';
 import './Login.scss'
+
+const API_BASE_URL = "http://localhost:10010/";
 
 export default class Login extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            username: "",
-            password: ""
+            username: "admin",
+            password: "password"
         };
     };
 
@@ -25,20 +28,44 @@ export default class Login extends Component {
 
     handleSubmit = event => {
         event.preventDefault();
+        const payload = {
+            "username": this.state.username,
+            "password": this.state.password,
+        };
+        const config = {
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        };
+        axios
+            .post(API_BASE_URL + 'login', payload, config)
+            .then(function (response) {
+                console.log(response);
+                if (response.data.code === 200) {
+                    console.log("200");
+                } else if (response.data.code === 204) {
+                    console.log("204");
+                } else {
+                    console.log("Username does not exists");
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     };
 
     render() {
         return (
             <div className={"box"}>
                 <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId={"username"} bsSize={"large"} className={"field"}>
+                    <Form.Group controlId={"username"} className={"field"}>
                         <Form.Control
                             autoFocus
                             value={this.state.username}
                             onChange={this.handleChange}
                         />
                     </Form.Group>
-                    <Form.Group controlId={"password"} bsSize={"large"} className={"field"}>
+                    <Form.Group controlId={"password"} className={"field"}>
                         <Form.Control
                             value={this.state.password}
                             onChange={this.handleChange}
@@ -47,7 +74,6 @@ export default class Login extends Component {
 
                         <Button
                             block
-                            bsSize={"large"}
                             disabled={!this.validateForm()}
                             type={"submit"}
                         >Login
